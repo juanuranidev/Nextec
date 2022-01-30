223c77891e59a7c60fae14738714db843e6a82d6
Juan Urani <96846723+juanuranidev@users.noreply.github.com>
2022-01-30 16:37:33 -0300

    Delete src/components/navbar directory

--- src/components/navbar/CloseMenuWidget/CloseMenuWidget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CloseMenuWidget({onClick}) {
-    return (
-        <span className="fas fa-times fa-2x closeMenu" onClick={onClick} />
-    )
-}
-
-export default CloseMenuWidget
