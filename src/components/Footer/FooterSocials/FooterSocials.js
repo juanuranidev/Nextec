a754a567720d6954894f27756ca6d9de03c97040
Juan Urani <juanuranidev@gmail.com>
2022-02-05 19:17:51 -0300

    updates

--- src/components/Footer/FooterSocials/FooterSocials.js
+++ src/components/Footer/FooterSocials/FooterSocials.js
@@ -7,8 +7,8 @@ const FooterSocials = () => {
       <h3 className='footerSocials_h3'>Redes Sociales</h3>
       <div className='footerSocials_div'>
         <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' className='footerSocials_div_a fab fa-youtube fa-3x' />
-        <a href='https://www.instagram.com/officialrickastley/' target='_blank' className='footerSocials_div_a fab fa-instagram fa-3x' />
-        <a href='https://twitter.com/rickastley' target='_blank' className='footerSocials_div_a fab fa-twitter fa-3x' />
+        <a href='https://www.instagram.com/therock/' target='_blank' className='footerSocials_div_a fab fa-instagram fa-3x' />
+        <a href='https://twitter.com/elonmusk' target='_blank' className='footerSocials_div_a fab fa-twitter fa-3x' />
       </div>
     </div>
   );
