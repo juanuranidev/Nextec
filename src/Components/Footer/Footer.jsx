c51fbe8421b1fa75f6c6b685bff1c003f7bdf35e
Juan Urani <juanuranidev@gmail.com>
2022-07-23 01:48:32 -0300

    Working on Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Newsletter from '../Newsletter/Newsletter';
+import Links from './Links/Links';
 import Info from './Info/Info';
-import './_Footer.scss';
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <Info/>
+      <div className='footer_info'>
+        <Info/>
+      </div>
+      <div className='footer_links'>
+        <Links/>
+      </div>
       <div className='footer_newsletter'>
         <Newsletter/>
       </div>
---
+++ src/Components/Footer/Links/Links.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Links.scss';
+
+const Links = () => {
+  return (
+    <div className='links'>
+        <h2 className='links_h2'>Importante</h2>
+        <p className='links_p'>Quiénes somos</p>
+        <p className='links_p'>Botón de arrepentimiento</p>
+        <p className='links_p'>Términos y condiciones</p>
+        <p className='links_p'>Politica de privacidad</p>
+        <p className='links_p'>Ventas corporativas</p>
+    </div>
+  );
+};
+
+export default Links;
