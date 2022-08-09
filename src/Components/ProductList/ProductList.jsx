b7cb42c242cc02f35e7a96407b2ba00c876822a3
Juan Urani <juanuranidev@gmail.com>
2022-08-08 23:04:25 -0300

    Working on Products

---
+++ src/Components/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductList = ({products}) => {
+  return (
+    <React.Fragment>
+    {products.map()}
+    </React.Fragment>
+  )
+}
+
+export default ProductList
