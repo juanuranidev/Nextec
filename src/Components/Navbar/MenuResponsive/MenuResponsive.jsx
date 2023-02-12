045457e8ae474dc3d47e661297b7863b08f895e4
juanuranidev <juanuranidev@gmail.com>
2023-02-12 00:24:44 -0500

    Working on MenuResponsive

--- src/Components/Navbar/MenuResponsive/MenuResponsive.jsx
+++ src/Components/Navbar/MenuResponsive/MenuResponsive.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MenuResponsive.scss";
 
 export default function MenuResponsive({ onOpen, onClose }) {
@@ -9,6 +10,28 @@ export default function MenuResponsive({ onOpen, onClose }) {
           className="fas fa-times fa-2x menuResponsive_close_span"
           onClick={onClose}
         />
+      </div>
+      <div className="menuResponsive_links">
+        <ul className="menuResponsive_links_ul">
+          <Link to="/" className="menuResponsive_links_ul_a" onClick={onClose}>
+            <li className="menuResponsive_links_ul_a_li">Inicio</li>
+          </Link>
+          <Link to="/" className="menuResponsive_links_ul_a" onClick={onClose}>
+            <li className="menuResponsive_links_ul_a_li">Productos</li>
+          </Link>
+          <Link to="/" className="menuResponsive_links_ul_a" onClick={onClose}>
+            <li className="menuResponsive_links_ul_a_li">Nosotros</li>
+          </Link>
+          <Link to="/" className="menuResponsive_links_ul_a" onClick={onClose}>
+            <li className="menuResponsive_links_ul_a_li">Contacto</li>
+          </Link>
+          <Link to="/" className="menuResponsive_links_ul_a" onClick={onClose}>
+            <li className="menuResponsive_links_ul_a_li">Arma tu PC</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="menuResponsive_bottom">
+        <p className="menuResponsive_bottom_p">Tech Store ® 2023</p>
       </div>
     </div>
   );
