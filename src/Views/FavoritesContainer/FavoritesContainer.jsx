efbc6dea72e8ffd35cc988cf3bc16827225ac856
juanuranidev <juanuranidev@gmail.com>
2023-03-06 23:17:25 -0300

    Working on favorites

---
+++ src/Views/FavoritesContainer/FavoritesContainer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useFavoritesContext } from "../../Context/FavoritesContext";
+import "./_FavoritesContainer.scss";
+
+const FavoritesContainer = () => {
+  const { favorites } = useFavoritesContext();
+
+  if (favorites.length === 0) {
+    return {
+      /* <Message h2={"No tenés favoritos"} p={"¡Agrega nuevos productos!"}  */
+    };
+  }
+
+  return (
+    <section className="favoritesContainer">
+      {/* <BackButton /> */}
+      <ItemList products={favorites} />
+    </section>
+  );
+};
+
+export default FavoritesContainer;
