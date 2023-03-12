69a09e13000907b10decf3dd13c1fb37d5d0ff48
juanuranidev <juanuranidev@gmail.com>
2023-03-11 23:59:27 -0300

    Working on ItemDetail

--- src/Views/ItemDetail/ItemDetail.jsx
+++ src/Views/ItemDetail/ItemDetail.jsx
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail/ItemDetail";
 import Loader from "../../Components/Loader/Loader";
 import "./ItemDetail.scss";
+import { useFavoritesContext } from "../../Context/FavoritesContext";
 
 const ItemDetailContainer = () => {
   const [item, setItem] = useState({});
   const [loading, setLoading] = useState(true);
   const { idItem } = useParams();
+  const { favorites, handleFavorites } = useFavoritesContext();
 
   const handleGetItemDetail = async () => {
     setLoading(true);
@@ -21,6 +23,12 @@ const ItemDetailContainer = () => {
     setLoading(false);
   };
 
+  function isInFavorites(item) {
+    return favorites.some(function (x) {
+      return x.id === item.id;
+    });
+  }
+
   useEffect(() => {
     handleGetItemDetail();
   }, [idItem]);
@@ -32,6 +40,23 @@ const ItemDetailContainer = () => {
       {/* <ItemDetail product={product} /> */}
       <div className="itemDetail_container">
         <img className="itemDetail_container_img" src={item.image} />
+        <div className="itemDetail_container_div">
+          {isInFavorites(item) ? (
+            <div className="itemDetail_container_div_favorite">
+              <span
+                className="fas fa-heart fa-2x itemDetail_container_div_favorite_span"
+                onClick={() => handleFavorites(item)}
+              />
+            </div>
+          ) : (
+            <div className="itemDetail_container_div_favorite">
+              <span
+                className="far fa-heart fa-2x itemDetail_container_div_favorite_span"
+                onClick={() => handleFavorites(item)}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
